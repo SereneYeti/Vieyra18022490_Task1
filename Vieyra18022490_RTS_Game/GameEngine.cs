c922e01b44521c97503759dda22f8f45d05a5448
SereneYeti <40300212+SereneYeti@users.noreply.github.com>
2019-10-07 14:10:28 +0200

    Fixed Melee Unit Class

---
+++ Vieyra18022490_RTS_Game/GameEngine.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vieyra18022490_RTS_Game
+{
+    class GameEngine
+    {   //Game Engine Class, Used to deal with all the processing and game logic of the game.
+        public int numRounds = 0;        
+        MeleeUnit melee = new MeleeUnit();
+        RangedUnit ranged = new RangedUnit();
+        Map map = new Map();
+        Random r = new Random();
+
+        public void GameLogic(Label label)
+        {   //Method used to process the ingame logic necessary for the game to run. Such as finding the closest units to attack and increasing the game round.
+            label.Text = "Round: " + numRounds;    
+            int unit1PosX = -1;
+            int unit1PosY = -1;
+            int unit2PosX = -1;
+            int unit2PosY = -1;
+            
+            for (int i = 0; i < 20; i++)
+            {
+                for(int j = 0; j < 20; j++)
+                {
+                    if((unit1PosX == -1) && (unit1PosY == -1) && (map.grid[i,j] == true))   //Find Closest
+                    {
+                        unit1PosX = i;
+                        unit1PosY = j;
+                        
+                    }
+                    if((unit1PosX != -1) && (unit1PosY != -1) && (map.grid[i, j] == true))  //Find Closest
+                    {
+                        unit2PosX = i;
+                        unit2PosY = j;
+                    }
+                    foreach(RangedUnit R in map.rangedUnits)    //Run Away
+                    {
+                        if(R.Health < 2)
+                        {
+                            ranged.Move((Direction)r.Next(0, 5), 1);
+                        }
+
+                    }
+                    foreach (MeleeUnit m in map.meleeUnits)//Run Away
+                    {
+                        if (m.Health < 2)
+                        {
+                            ranged.Move((Direction)r.Next(0, 5), 1);
+                        }
+                        
+                    }
+                                        
+                }
+            }
+
+            numRounds++;
+        }
+
+        
+    }
+}
---
+++ Vieyra18022490_RTS_Game/Unit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vieyra18022490_RTS_Game
+{   //Abstract class with no code implemented
+    //Used for the sake of re-use and inheritance
+    public enum Direction
+    {
+        North,
+        South,
+        East,
+        West
+    }
+    abstract class Unit
+    {
+        
+        protected int xPos { get; set; }
+        protected int yPos { get; set; }
+        protected int health { get; set; }
+        protected int maxHealth { get; }
+        protected int speed { get; set; }
+        protected int attack { get; set; }
+        protected int attackRange { get; set; }
+        protected string symbol { get; set; }
+        protected string faction { get; set; }
+        protected bool attacking { get; set; }
+
+        public abstract void Move(Direction d,int speed);
+
+        public abstract void Attack(Unit attacker);
+
+        public abstract bool inRange(Unit other);
+
+        public abstract int Closest(int x, int y);
+
+        public abstract void Death();
+
+        public abstract override string ToString();
+        
+    }
+}
